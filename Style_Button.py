366d81296d715b3ce5a7ef9d11514adfd7882c6f
Slava-space <slavapashin5@gmail.com>
2024-10-21 16:16:41 +0300

    Style of Button

---
+++ Style_Button.py
@@ -0,0 +1,15 @@
+QLineEdit:focus {
+                border-color: #5d8bb8;
+            }
+            QPushButton {
+                background-color: #5d8bb8;
+                color: white;
+                font-size: 16px;
+                padding: 12px;
+                border-radius: 10px;
+                box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.3);
+            }
+            QPushButton:hover {
+                background-color: #4a7498;
+            }
+        """)
